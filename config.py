005b72d90cfd70a853ad56d12506cb46169563a3
VIGNESH RATHOD <105590547+VIGGU-7@users.noreply.github.com>
2025-01-10 22:47:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 baseurl="https://panel.pterodactyl.in/api/application"
 url="https://panel.pterodactyl.in/api/"
 client="client"
-model="gpt-4o"
 apikey="ptla--"
 def getapikey():
     return apikey
